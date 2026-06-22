f5566337b69ea0e374887d6389b438122f086d54
jcvheerden <ST10316473@vcconnect.edu.za>
2026-06-22 17:18:09 +0200

    Added code to git folder
    added the first commit to start the program

---
+++ GitHub/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SmallConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome to the C# Console App!");
+            
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"\nHello, {name}! Let's do a quick calculation.");
+             
+            Console.Write("Enter a whole number: ");
+            string input = Console.ReadLine();
+            
+            if (int.TryParse(input, out int number))
+            {
+                int result = number * 2;
+                Console.WriteLine($"Double your number is: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Oops! That wasn't a valid number.");
+            }
+ 
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
